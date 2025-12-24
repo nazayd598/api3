bd38247d4f25260f8da19eb9bf31d423a6b8fa1d
nazayd598 <aydemirnaz1938@gmail.com>
2025-12-25 01:13:54 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import { getBookQuote } from "./services/quote.js";
-
 document.getElementById("btn").addEventListener("click", async () => {
   const book = document.getElementById("book").value;
-  const data = await getBookQuote(book);
+
+  const res = await fetch(`/api/book?name=${book}`);
+  const data = await res.json();
 
   document.getElementById("result").innerText =
-    `${data.book} - ${data.author}\n"${data.quote}"`;
+    `${data.title} - ${data.author}\n"${data.quote}"`;
 });
