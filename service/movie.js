3b7983d819d8bfe17779f377ac60fe5238b6f0f6
nazayd598 <aydemirnaz1938@gmail.com>
2025-12-25 09:45:24 +0300

    Update movie.js

--- service/movie.js
+++ service/movie.js
@@ -1,14 +1,23 @@
-import { getBookInfo } from "./book.js";
+const genreMap = {
+  Fantasy: "Fantasy",
+  Adventure: "Adventure",
+  Fiction: "Fantasy",
+  Romance: "Drama"
+};
 
-export async function getBookQuote(bookName) {
-  const book = await getBookInfo(bookName);
+export async function getMovieByGenre(genre) {
+  const res = await fetch("https://ghibliapi.vercel.app/films");
+  const movies = await res.json();
 
-  const res = await fetch("https://api.quotable.io/random");
-  const quote = await res.json();
+  const mappedGenre = genreMap[genre] || "Fantasy";
+
+  const movie = movies.find(m =>
+    m.description.toLowerCase().includes(mappedGenre.toLowerCase())
+  ) || movies[0];
 
   return {
-    book: book.title,
-    author: book.author,
-    quote: quote.content
+    title: movie.title,
+    director: movie.director,
+    year: movie.release_date
   };
 }
